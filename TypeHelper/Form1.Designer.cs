3e786d404a441ab6cce358359d5291e6ca9e8e79
phillyai <sonagi.philly@gmail.com>
2017-12-06 13:53:23 +0900

    초기 커밋

--- TypeHelper/Form1.Designer.cs
+++ TypeHelper/Form1.Designer.cs
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.radAll = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radOne = new System.Windows.Forms.RadioButton();
+            this.radLine = new System.Windows.Forms.RadioButton();
+            this.combTitle = new System.Windows.Forms.ComboBox();
+            this.trackSpeed = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radAll
+            // 
+            this.radAll.AutoSize = true;
+            this.radAll.Location = new System.Drawing.Point(18, 24);
+            this.radAll.Name = "radAll";
+            this.radAll.Size = new System.Drawing.Size(126, 25);
+            this.radAll.TabIndex = 0;
+            this.radAll.TabStop = true;
+            this.radAll.Text = "전 부치기";
+            this.radAll.UseVisualStyleBackColor = true;
+            this.radAll.CheckedChanged += new System.EventHandler(this.radMods_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radOne);
+            this.groupBox1.Controls.Add(this.radLine);
+            this.groupBox1.Controls.Add(this.radAll);
+            this.groupBox1.Location = new System.Drawing.Point(20, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "모드 선택";
+            // 
+            // radOne
+            // 
+            this.radOne.AutoSize = true;
+            this.radOne.Location = new System.Drawing.Point(18, 84);
+            this.radOne.Name = "radOne";
+            this.radOne.Size = new System.Drawing.Size(154, 25);
+            this.radOne.TabIndex = 0;
+            this.radOne.TabStop = true;
+            this.radOne.Text = "한 타당 한자";
+            this.radOne.UseVisualStyleBackColor = true;
+            this.radOne.CheckedChanged += new System.EventHandler(this.radMods_CheckedChanged);
+            // 
+            // radLine
+            // 
+            this.radLine.AutoSize = true;
+            this.radLine.Location = new System.Drawing.Point(18, 55);
+            this.radLine.Name = "radLine";
+            this.radLine.Size = new System.Drawing.Size(168, 25);
+            this.radLine.TabIndex = 0;
+            this.radLine.TabStop = true;
+            this.radLine.Text = "페이지 씩치기";
+            this.radLine.UseVisualStyleBackColor = true;
+            this.radLine.CheckedChanged += new System.EventHandler(this.radMods_CheckedChanged);
+            // 
+            // combTitle
+            // 
+            this.combTitle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combTitle.FormattingEnabled = true;
+            this.combTitle.Location = new System.Drawing.Point(237, 21);
+            this.combTitle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.combTitle.Name = "combTitle";
+            this.combTitle.Size = new System.Drawing.Size(253, 29);
+            this.combTitle.TabIndex = 2;
+            // 
+            // trackSpeed
+            // 
+            this.trackSpeed.AutoSize = false;
+            this.trackSpeed.Location = new System.Drawing.Point(237, 64);
+            this.trackSpeed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.trackSpeed.Maximum = 1000;
+            this.trackSpeed.Minimum = 1;
+            this.trackSpeed.Name = "trackSpeed";
+            this.trackSpeed.Size = new System.Drawing.Size(253, 32);
+            this.trackSpeed.TabIndex = 3;
+            this.trackSpeed.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackSpeed.Value = 50;
+            this.trackSpeed.Scroll += new System.EventHandler(this.trackSpeed_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(237, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "500타";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(510, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(80, 133);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 80;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(568, 154);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackSpeed);
+            this.Controls.Add(this.combTitle);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("굴림", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "TypeHelper";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radAll;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radOne;
+        private System.Windows.Forms.RadioButton radLine;
+        private System.Windows.Forms.ComboBox combTitle;
+        private System.Windows.Forms.TrackBar trackSpeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- TypeHelper/Form1.cs
+++ TypeHelper/Form1.cs
@@ -4,17 +4,130 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TypeHelper
 {
     public partial class Form1 : Form
     {
+        enum Mod { All, Line, One }
+        Mod currentMod;
+        int speed = 100;
+        bool systemTyping = false;
+        int index, inindex;
+
+        bool started = false;
+        ManualResetEvent waiter;
+
+        Dictionary<string, string[]> texts;
+
         public Form1()
         {
             InitializeComponent();
+
+            texts = new Dictionary<string, string[]>();
+            foreach (var file in new DirectoryInfo("datas").EnumerateFiles("*.txt"))
+            {
+                var name = Path.GetFileNameWithoutExtension(file.Name);
+                texts.Add(name, File.ReadAllLines(file.FullName));
+                combTitle.Items.Add(name);
+            }
+
+            waiter = new ManualResetEvent(false);
+            combTitle.SelectedIndex = 0;
+            trackSpeed.Value = 50;
+            KeyboardHook.KeyDown += KeyboardHook_KeyDown;
+            KeyboardHook.HookStart();
+            trackBar1_Scroll(null, null);
+        }
+
+        private bool KeyboardHook_KeyDown(Keys key)
+        {
+            if (systemTyping) return true;
+            if (key == Keys.Back) return true;
+            if (key == Keys.F2) Start();
+            else if (key == Keys.F3) Finish();
+            else if (key == Keys.F10)
+            {
+                if (ModifierKeys == Keys.Control)
+                    Show();
+                else Hide();
+            }
+            if (!started) return true;
+
+            var cur = texts[combTitle.Text];
+            switch (currentMod)
+            {
+                case Mod.All:
+                    return false;
+                case Mod.Line:
+                    systemTyping = true;
+                    if (index < cur.Length)
+                    {
+                        SendKeys.SendWait(cur[index] + "~");
+                        SendKeys.SendWait(cur[index+1] + "~");
+                        SendKeys.SendWait(cur[index+2] + "~");
+                        SendKeys.SendWait(cur[index+3] + "~");
+                        SendKeys.SendWait(cur[index+4] + "~");
+                    }index += 5;
+                    systemTyping = false;
+                    break;
+                case Mod.One:
+                    if (inindex == cur[index].Length)
+                    {
+                        index++;
+                        inindex = 0;
+                        systemTyping = true;
+                        SendKeys.SendWait("~");
+                        systemTyping = false;
+                        break;
+                    }
+                    if (index == cur.Length)
+                        break;
+                    systemTyping = true;
+                    SendKeys.SendWait(cur[index][inindex++].ToString());
+                    systemTyping = false;
+                    break;
+            }
+            return false;
+        }
+
+        void Start()
+        {
+            index = 0;
+            inindex = 0;
+            started = true;
+        }
+
+        void Finish()
+        {
+            started = false;
+            systemTyping = false;
+        }
+        
+        private void radMods_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radAll.Checked) currentMod = Mod.All;
+            else if (radLine.Checked) currentMod = Mod.Line;
+            else if (radOne.Checked) currentMod = Mod.One;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            KeyboardHook.HookEnd();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = trackBar1.Value / 100.0;
+        }
+
+        private void trackSpeed_Scroll(object sender, EventArgs e)
+        {
+            speed = trackSpeed.Value;
+            label1.Text = $"{speed}타";
         }
     }
 }
